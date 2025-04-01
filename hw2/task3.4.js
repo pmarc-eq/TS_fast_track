dae19c86918a59091a4a92aa3c8f945670a132a1
pmarc-eq <pmarc@equinor.com>
2025-04-01 13:15:43 +0200

    get numbers from user support added

--- hw2/task3.4.js
+++ hw2/task3.4.js
@@ -6,5 +6,8 @@ function raiseToDegree(a, b) {
     return a ** b
 }
 
+const prompt = require('prompt-sync')({sigint: true});
+const a = parseInt(prompt("Give a number to be raised: "))
+const b = parseInt(prompt("Give a power number: "))
 
-console.log(raiseToDegree(2, 3))
+console.log(raiseToDegree(a, b))

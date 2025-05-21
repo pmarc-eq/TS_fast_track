be24cb9914ac17ad8d17215bf15afeb40b38ffdc
pmarc-eq <pmarc@equinor.com>
2025-05-21 17:24:43 +0200

    some tiding up in task1 from hw7

--- hw7/tests/task1.spec.ts
+++ hw7/tests/task1.spec.ts
@@ -1,16 +1,13 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
+test('Verify example.com contents', async ({ page }) => {
     await page.goto("https://example.com/");
 
-    const title = await page.title();
-    expect(title).toBe("Example Domain");
+    expect(await page.title()).toBe("Example Domain");
     
     expect(page.url()).toContain("example.com");
 
-    const body_text = await page.textContent('body');
-    expect(body_text).toMatch(/This domain is for use in illustrative examples/);
+    expect(await page.textContent('body')).toMatch(/This domain is for use in illustrative examples/);
 
-    const h1_loc = page.locator('h1')
-    await expect(h1_loc).toHaveText("Example Domain");
+    await expect(page.locator('h1')).toHaveText("Example Domain");
 });
